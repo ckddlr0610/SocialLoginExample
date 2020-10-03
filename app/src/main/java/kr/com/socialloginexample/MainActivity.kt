394690c9bb545aca9175fbcb7d5576f691e4bae2
Teddy_changik <aackddlr@gmail.com>
2020-10-03 23:48:32 +0900

    init commit

---
+++ app/src/main/java/kr/com/socialloginexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package kr.com.socialloginexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.com.socialloginexample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/kr/com/socialloginexample/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package kr.com.socialloginexample.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
